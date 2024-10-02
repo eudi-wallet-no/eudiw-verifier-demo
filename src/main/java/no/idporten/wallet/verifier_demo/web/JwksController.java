1930dbc596906e53582316db0e7d8908eb6792a6
Thomas Reppesgård <thomas.reppesgard@digdir.no>
2024-10-02 12:13:05 +0200

    ID-4642: Walt.id + kode fra vcverifier POC (#4)
    * Keystores i profiler
    
    * Port fra Aamunds POC + benytte walti.d
    
    * https://demo-aldersverifisering.idporten.dev/
    
    * Update .trivyignore

---
+++ src/main/java/no/idporten/wallet/verifier_demo/web/JwksController.java
@@ -0,0 +1,33 @@
+package no.idporten.wallet.verifier_demo.web;
+
+import com.nimbusds.jose.jwk.JWK;
+import no.idporten.wallet.verifier_demo.crypto.JwksProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping(value = {"/jwk", "/jwks", "/jwk/", "/jwks/", "/.well-known/jwks.json", "/jwk.json", "/jwks/.well-known/jwks.json", "/jwks-endpoint", "/jwks-endpoint/", "/wallet/public-keys.json"})
+@CrossOrigin(origins = "*")
+public class JwksController {
+
+        private JwksProvider jwksProvider;
+
+        @Autowired
+        public JwksController(JwksProvider jwksProvider) {
+            this.jwksProvider = jwksProvider;
+        }
+
+        @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public ResponseEntity getJwk() {
+            return ResponseEntity.ok(JwkResponse.builder().addJwks(jwksProvider.jwks().getKeys().stream().map(JWK::toJSONString).collect(Collectors.toList())).build());
+        }
+
+}
---
+++ src/main/java/no/idporten/wallet/verifier_demo/web/ResponseController.java
@@ -0,0 +1,88 @@
+package no.idporten.wallet.verifier_demo.web;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.crypto.RSADecrypter;
+import id.walt.mdoc.SimpleCOSECryptoProvider;
+import id.walt.mdoc.dataelement.DataElement;
+import id.walt.mdoc.dataelement.EncodedCBORElement;
+import id.walt.mdoc.dataelement.MapElement;
+import id.walt.mdoc.dataelement.MapKey;
+import id.walt.mdoc.dataretrieval.DeviceResponse;
+import id.walt.mdoc.doc.MDoc;
+import id.walt.mdoc.issuersigned.IssuerSigned;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.idporten.wallet.verifier_demo.crypto.KeyProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.security.interfaces.RSAPrivateKey;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+class ResponseController {
+
+    private final KeyProvider keyProvider;
+
+
+    @ResponseBody
+    @PostMapping("/response")
+    public void handleResponse(@ModelAttribute(name = "response") String response, @ModelAttribute(name = "state") String state, @ModelAttribute(name = "vp_token") String vpToken) throws ParseException, JOSEException, IOException {
+
+        Map<String, Object> claimsFromJwePayload = decryptAndDeserializeJweResponse(response);
+
+        Map<String, String> elementsFromPidDocumentInMDoc = retrieveElementsFromPidDocumentInMDoc((String) claimsFromJwePayload.get("vp_token"));
+
+        log.info("Received authorization response from wallet");
+        log.info("Got following elements from PID-document:");
+        elementsFromPidDocumentInMDoc.keySet().stream()
+                .forEach(k -> log.info(k + ": "+elementsFromPidDocumentInMDoc.get(k)));
+    }
+
+    private Map<String, Object> decryptAndDeserializeJweResponse(String response) throws IOException, ParseException, JOSEException {
+
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyProvider.privateKey();
+        RSADecrypter decrypter = new RSADecrypter(privateKey);
+
+        JWEObject jwe = JWEObject.parse(response);
+        jwe.decrypt(decrypter);
+
+        return jwe.getPayload().toJSONObject();
+    }
+
+    private Map<String, String> retrieveElementsFromPidDocumentInMDoc(String vpToken) throws IOException {
+        SimpleCOSECryptoProvider cryptoProvider = new SimpleCOSECryptoProvider(List.of()); // TODO hvor kommer nøkler fra og issuer key id?
+        DeviceResponse deviceResponse = DeviceResponse.Companion.fromCBORBase64URL(vpToken);
+        for (MDoc mDoc : deviceResponse.getDocuments()) {
+
+
+
+            System.out.println("docType: " + mDoc.getDocType().getValue());
+            mDoc.getMSO(); // TODO verify med hvilke nøkler?
+            mDoc.verifyDocType();
+            mDoc.verifyIssuerSignedItems();
+            mDoc.verifyValidity();
+            IssuerSigned issuerSigned = mDoc.getIssuerSigned();
+            for (String namespace : issuerSigned.getNameSpaces().keySet()) {
+                log.info("namespace = " + namespace);
+                List<EncodedCBORElement> elements = issuerSigned.getNameSpaces().get(namespace);
+                for (EncodedCBORElement element : elements) {
+                    Map<MapKey, DataElement> elementMap = ((MapElement) element.decode()).getValue();
+                    for(MapKey mapKey : elementMap.keySet()) {
+                        log.info(mapKey.toString() + "=" + elementMap.get(mapKey).getInternalValue());
+                    }
+                }
+            }
+        }
+        return Map.of("TODO", "walt.id");
+    }
+
+}
