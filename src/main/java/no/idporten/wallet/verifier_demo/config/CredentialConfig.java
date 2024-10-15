0071400c0a84a122088cb802d38f2e6a9835229d
Thomas Reppesgård <thr@digdir.no>
2024-10-15 09:25:05 +0200

    Config bevistyper

---
+++ src/main/java/no/idporten/wallet/verifier_demo/config/CredentialConfig.java
@@ -0,0 +1,25 @@
+package no.idporten.wallet.verifier_demo.config;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Validated
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CredentialConfig {
+
+    @NotNull
+    private String id;
+    @NotNull
+    private String docType;
+    @NotEmpty
+    private List<String> fields;
+
+}
