fddb117f0b317b1113b29dd7191080378541fe64
Thomas Reppesgård <thr@digdir.no>
2025-04-10 07:44:21 +0200

    Oppdatere libs + sikre bouncy castle provider installert

--- src/main/java/no/idporten/eudiw/demo/verifier/VerifierDemoApplication.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/VerifierDemoApplication.java
@@ -3,11 +3,21 @@ package no.idporten.eudiw.demo.verifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class VerifierDemoApplication {
 
 	public static void main(String[] args) {
+		addBouncyCastleProvider();
 		SpringApplication.run(VerifierDemoApplication.class, args);
+	}
+
+	/**
+	 * Bootstrap Bouncy Castle.
+	 */
+	private static void addBouncyCastleProvider() {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 	}
 
 }
