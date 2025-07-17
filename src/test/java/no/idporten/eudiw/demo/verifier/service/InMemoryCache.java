e0668a0df9947fdca56bcd2a106a7c10871446d7
Thomas Reppesgård <thr@digdir.no>
2025-07-17 14:53:38 +0200

    Cacheservice med ulike implementasjoner av cache (redis, in-memory)

---
+++ src/test/java/no/idporten/eudiw/demo/verifier/service/InMemoryCache.java
@@ -0,0 +1,32 @@
+package no.idporten.eudiw.demo.verifier.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * In-memory cache for bruk i enhetstester.
+ */
+@Service
+public class InMemoryCache implements Cache {
+
+    private final Map<String, Object> cache = new HashMap<>();
+
+    @Override
+    public void set(String cacheKey, Object object, Duration duration) {
+        cache.put(cacheKey, object);
+    }
+
+    @Override
+    public Object get(String cacheKey) {
+        return cache.get(cacheKey);
+    }
+
+    @Override
+    public Object remove(String cacheKey) {
+        return cache.remove(cacheKey);
+    }
+
+}
