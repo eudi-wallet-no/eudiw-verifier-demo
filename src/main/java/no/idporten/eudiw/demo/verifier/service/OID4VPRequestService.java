7cef4a6032477f173973e1036390dd1a25ff1c96
Thomas Reppesgård <thr@digdir.no>
2025-10-06 14:53:49 +0200

    DCQL + nytt keystore-lib

--- src/main/java/no/idporten/eudiw/demo/verifier/service/OID4VPRequestService.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/service/OID4VPRequestService.java
@@ -2,10 +2,11 @@ package no.idporten.eudiw.demo.verifier.service;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.ECDSASigner;
-import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.*;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.util.Base64;
-import com.nimbusds.jose.util.JSONObjectUtils;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
@@ -16,18 +17,24 @@ import net.minidev.json.JSONObject;
 import no.idporten.eudiw.demo.verifier.config.ConfigProvider;
 import no.idporten.eudiw.demo.verifier.config.CredentialConfig;
 import no.idporten.eudiw.demo.verifier.crypto.ECUtils;
-import no.idporten.eudiw.demo.verifier.crypto.KeyProvider;
+import no.idporten.lib.keystore.KeyProvider;
+import no.idporten.lib.keystore.KeystoreManager;
 import org.springframework.stereotype.Service;
 
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.time.Clock;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 public class OID4VPRequestService {
 
     private final ConfigProvider configProvider;
-    private final KeyProvider keyProvider;
+    private final KeystoreManager keystoreManager;
 
     public String getAuthorizationRequest(String type, String state) {
         return "eudi-openid4vp://"
@@ -39,6 +46,7 @@ public class OID4VPRequestService {
 
     public JWT makeRequestJwt(String type, String state) throws Exception {
         CredentialConfig credentialConfig = configProvider.getCredentialConfig(type);
+        KeyProvider keyProvider = keystoreManager.getKeyProvider("access");
         List<Base64> certChain = new ArrayList<>();
         certChain.add(com.nimbusds.jose.util.Base64.encode(keyProvider.certificate().getEncoded()));
         JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder()
@@ -50,8 +58,6 @@ public class OID4VPRequestService {
                 .claim("nonce", UUID.randomUUID().toString())
                 .claim("state", state)
                 .claim("client_id", configProvider.getClientIdentifier())
-//                .claim("client_id_scheme", configProvider.getClientIdentifierScheme())
-//                .claim("presentation_definition", makePresentationDefinition(credentialConfig))
                 .claim("dcql_query", makeDCQL(credentialConfig))
                 .claim("client_metadata", makeClientMetadata())
                 .jwtID(UUID.randomUUID().toString()) // Must be unique for each grant
@@ -60,19 +66,11 @@ public class OID4VPRequestService {
         JWTClaimsSet claims = builder.build();
         final JWSHeader jwtHeader;
         final JWSSigner signer;
-        if (keyProvider.isRsa()) {
-            jwtHeader = new JWSHeader.Builder(JWSAlgorithm.RS256)
-                    .x509CertChain(certChain)
-                    .type(new JOSEObjectType("oauth-authz-req+jwt"))
-                    .build();
-            signer = new RSASSASigner(keyProvider.privateKey());
-        } else {
-            jwtHeader = new JWSHeader.Builder(ECUtils.jwsAlgorithmFromKey(keyProvider))
-                    .x509CertChain(certChain)
-                    .type(new JOSEObjectType("oauth-authz-req+jwt"))
-                    .build();
-            signer = new ECDSASigner(keyProvider.ecPrivateKey());
-        }
+        jwtHeader = new JWSHeader.Builder(ECUtils.jwsAlgorithmFromKey(keyProvider.publicKey()))
+                .x509CertChain(certChain)
+                .type(new JOSEObjectType("oauth-authz-req+jwt"))
+                .build();
+        signer = new ECDSASigner((ECPrivateKey) keyProvider.privateKey());
         SignedJWT signedJWT = new SignedJWT(jwtHeader, claims);
         signedJWT.sign(signer);
         return signedJWT;
@@ -80,80 +78,25 @@ public class OID4VPRequestService {
 
     @SneakyThrows
     public JSONObject makeDCQL(CredentialConfig credentialConfig) {
-        JSONObject pd = new JSONObject();
-        pd.appendField("id", UUID.randomUUID().toString());
-//        pd.appendField("input_descriptors", makeInputDescriptors(credentialConfig));
-        // TODO dette skal gjøres ordentlig
-        String dcqlQuery = """
-                {
-                  "credentials": [
-                    {
-                      "id": "%s",
-                      "format": "mso_mdoc",
-                      "meta": {
-                        "doctype_value": "%s"
-                      },
-                      "claims": [
-                        {
-                          "path": [
-                            "%s", "%s"
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }""".formatted(credentialConfig.getId(), credentialConfig.getDocType(), credentialConfig.getDocType(), credentialConfig.getFields().getFirst());
-        return new JSONObject(JSONObjectUtils.parse(dcqlQuery));
-    }
-
-    private JSONObject makePresentationDefinition(CredentialConfig credentialConfig) {
-        JSONObject pd = new JSONObject();
-        pd.appendField("id", UUID.randomUUID().toString());
-        pd.appendField("input_descriptors", makeInputDescriptors(credentialConfig));
-        return pd;
-    }
-
-    private JSONArray makeInputDescriptors(CredentialConfig credentialConfig) {
-        JSONArray ids = new JSONArray(1);
-        ids.add(makeInputDescriptor(credentialConfig.getDocType(), credentialConfig.getFields()));
-        return ids;
-    }
-
-    private JSONObject makeInputDescriptor(String docType, List<String> attributes) {
-        JSONObject descriptor = new JSONObject();
-        descriptor.appendField("id", docType);
-        descriptor.appendField("name", "EUDI PID");
-        descriptor.appendField("purpose", "We need to verify your identity");
-        descriptor.appendField("format", makeVpFormatsSupported());
-        descriptor.appendField("constraints", makeConstraints(docType, attributes));
-        return descriptor;
-    }
-
-
-    public JSONObject makeConstraints(String docType, List<String> attributes) {
-        JSONObject constraints = new JSONObject();
-        JSONArray fields = new JSONArray();
-        for (String attribute : attributes) {
-            fields.add(makeField(docType, attribute, false));
-            constraints.appendField("fields", fields);
+        JSONObject credential = new JSONObject()
+                .appendField("id", credentialConfig.getId())
+                .appendField("format", "mso_mdoc")
+                .appendField("meta", new JSONObject().appendField("doctype_value", credentialConfig.getDocType()));
+        JSONArray claims = new JSONArray();
+        for (String field : credentialConfig.getFields()) {
+            claims.appendElement(new JSONObject()
+                    .appendField("path",
+                            new JSONArray()
+                                    .appendElement(credentialConfig.getDocType())
+                                    .appendElement(field)));
         }
-        return constraints;
+        credential.put("claims", claims);
+        JSONObject dcql = new JSONObject().appendField("credentials", new JSONArray().appendElement(credential));
+        return dcql;
     }
 
-    public JSONObject makeField(String docType, String claim, boolean retain) {
-        JSONObject field = new JSONObject();
-        field.appendField("path", makePath(docType, claim));
-        field.appendField("intent_to_retain", retain);
-        return field;
-    }
-
-    public JSONArray makePath(String docType, String claim) {
-        JSONArray path = new JSONArray(1);
-        path.add("$['" + docType + "']['" + claim + "']");
-        return path;
-    }
-
-    public JSONObject makeVpFormats() {
+    @Deprecated
+    private JSONObject makeVpFormats() {
         JSONArray algs = new JSONArray();
         algs.add(JWSAlgorithm.RS256.getName());
         algs.add(JWSAlgorithm.ES256.getName());
@@ -165,24 +108,24 @@ public class OID4VPRequestService {
         return format;
     }
 
-    public JSONObject makeVpFormatsSupported() {
+    private JSONObject makeVpFormatsSupported() {
         JSONObject mdoc = new JSONObject();
         JSONObject format = new JSONObject();
         format.appendField("mso_mdoc", mdoc);
         return format;
     }
 
-    public JSONObject makeClientMetadata() {
+    private JSONObject makeClientMetadata() {
         JSONObject metadata = new JSONObject();
         metadata.appendField("jwks", makeJwks().toPublicJWKSet().toJSONObject());
-        // JARM Android v24
-        metadata.appendField("id_token_signed_response_alg", JWSAlgorithm.RS256.getName());
-        metadata.appendField("authorization_encrypted_response_alg", JWEAlgorithm.ECDH_ES.getName());
-        metadata.appendField("authorization_encrypted_response_enc", EncryptionMethod.A128CBC_HS256.getName());
         JSONArray encryptedResponseAlgs = new JSONArray();
         encryptedResponseAlgs.add(EncryptionMethod.A128GCM.getName());
         metadata.appendField("encrypted_response_enc_values_supported", encryptedResponseAlgs);
         metadata.appendField("vp_formats_supported", makeVpFormatsSupported());
+        // JARM Android v24
+        metadata.appendField("id_token_signed_response_alg", JWSAlgorithm.RS256.getName());
+        metadata.appendField("authorization_encrypted_response_alg", JWEAlgorithm.ECDH_ES.getName());
+        metadata.appendField("authorization_encrypted_response_enc", EncryptionMethod.A128CBC_HS256.getName());
         // VP formats Android v24
         metadata.appendField("vp_formats", makeVpFormats());
         return metadata;
@@ -191,19 +134,12 @@ public class OID4VPRequestService {
     @SneakyThrows
     private JWKSet makeJwks() {
         List<JWK> jwkList = new ArrayList<>();
-        if (keyProvider.isRsa()) {
-            jwkList.add(new RSAKey.Builder(keyProvider.rsaPublicKey())
-                    .keyUse(KeyUse.ENCRYPTION)
-                    .keyIDFromThumbprint()
-
-                    .build());
-        } else {
-            jwkList.add(new ECKey.Builder(ECUtils.curveFromKey(keyProvider), keyProvider.ecPublicKey())
-                    .keyUse(KeyUse.ENCRYPTION)
-                    .keyIDFromThumbprint()
-                    .algorithm(JWEAlgorithm.ECDH_ES)
-                    .build());
-        }
+        ECPublicKey publicKey = (ECPublicKey) keystoreManager.getKeyProvider("access").publicKey();
+        jwkList.add(new ECKey.Builder(ECUtils.curveFromKey(publicKey), publicKey)
+                .keyUse(KeyUse.ENCRYPTION)
+                .keyIDFromThumbprint()
+                .algorithm(JWEAlgorithm.ECDH_ES)
+                .build());
         return new JWKSet(jwkList);
     }
 
