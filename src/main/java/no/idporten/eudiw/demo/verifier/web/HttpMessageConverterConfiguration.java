3e24fa27d831fc576c38f98f0f6e8a46c795ed84
Thomas Reppesgård <thr@digdir.no>
2025-11-26 10:39:52 +0100

    Flytt message converter til web

--- src/main/java/no/idporten/eudiw/demo/verifier/web/HttpMessageConverterConfiguration.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/web/HttpMessageConverterConfiguration.java
@@ -1,4 +1,4 @@
-package no.idporten.eudiw.demo.verifier.config;
+package no.idporten.eudiw.demo.verifier.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
