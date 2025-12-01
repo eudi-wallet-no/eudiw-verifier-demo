305ed38b52c8a6ad8f6014d3ceea8ecaadbf1f35
Thomas Reppesgård <thomas.reppesgard@digdir.no>
2025-12-01 15:35:10 +0100

    EUW-917: Språkstøtte en + no (#185)

--- src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
@@ -1,13 +1,9 @@
 package no.idporten.eudiw.demo.verifier.web;
 
-import jakarta.servlet.http.HttpServletRequest;
 import no.idporten.eudiw.demo.verifier.config.ConfigProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -19,7 +15,7 @@ public class IndexController {
     }
 
     @GetMapping(value = "/")
-    public String index(Model model, @RequestHeader Map<String, String> headers, HttpServletRequest request) {
+    public String index(Model model) {
         model.addAttribute("credentialTypes", configProvider.getCredentialConfigurations());
         return "index";
     }
