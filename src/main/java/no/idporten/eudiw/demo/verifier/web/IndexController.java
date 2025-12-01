e51bb2be6190c04850939779ee34c4e5ab6ad3cd
Thomas Reppesgård <thr@digdir.no>
2025-12-01 15:09:17 +0100

    Fix æøå

--- src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
@@ -1,32 +1,21 @@
 package no.idporten.eudiw.demo.verifier.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import no.idporten.eudiw.demo.verifier.config.ConfigProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.LocaleResolver;
-
-import java.util.Locale;
 
 @Controller
 public class IndexController {
 
     private final ConfigProvider configProvider;
-    private final LocaleResolver localeResolver;
 
-    public IndexController(ConfigProvider configProvider, LocaleResolver localeResolver) {
+    public IndexController(ConfigProvider configProvider) {
         this.configProvider = configProvider;
-        this.localeResolver = localeResolver;
     }
 
     @GetMapping(value = "/")
-    public String index(@RequestParam(name = "lang", required = false) String locale, Model model, HttpServletRequest request, HttpServletResponse response) {
-        if (locale != null) {
-            localeResolver.setLocale(request, response, "en".equals(locale) ? Locale.ENGLISH : Locale.of("no"));
-        }
+    public String index(Model model) {
         model.addAttribute("credentialTypes", configProvider.getCredentialConfigurations());
         return "index";
     }
