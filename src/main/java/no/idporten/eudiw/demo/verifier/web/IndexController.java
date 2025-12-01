db938946410ba1f0ee6415fd9c0ac248121f0504
Thomas Reppesgård <thr@digdir.no>
2025-12-01 12:58:20 +0100

    Konfig språkstøtte og mulighet for å bytte språk

--- src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
@@ -1,25 +1,32 @@
 package no.idporten.eudiw.demo.verifier.web;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import no.idporten.eudiw.demo.verifier.config.ConfigProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 
-import java.util.Map;
+import java.util.Locale;
 
 @Controller
 public class IndexController {
 
     private final ConfigProvider configProvider;
+    private final LocaleResolver localeResolver;
 
-    public IndexController(ConfigProvider configProvider) {
+    public IndexController(ConfigProvider configProvider, LocaleResolver localeResolver) {
         this.configProvider = configProvider;
+        this.localeResolver = localeResolver;
     }
 
     @GetMapping(value = "/")
-    public String index(Model model, @RequestHeader Map<String, String> headers, HttpServletRequest request) {
+    public String index(@RequestParam(name = "lang", required = false) String locale, Model model, HttpServletRequest request, HttpServletResponse response) {
+        if (locale != null) {
+            localeResolver.setLocale(request, response, "en".equals(locale) ? Locale.ENGLISH : Locale.of("no"));
+        }
         model.addAttribute("credentialTypes", configProvider.getCredentialConfigurations());
         return "index";
     }
