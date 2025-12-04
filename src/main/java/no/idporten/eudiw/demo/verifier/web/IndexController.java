64cbbcb47bc04832cfec309805f1b07deb6c8495
Magnus Lefdal <magnus.lefdal@digdir.no>
2025-12-04 10:52:58 +0100

    EUW-967: nå med interceptor (#190)
    nå med interceptor

--- src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/web/IndexController.java
@@ -1,34 +1,22 @@
 package no.idporten.eudiw.demo.verifier.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import no.idporten.eudiw.demo.verifier.config.ConfigProvider;
-import no.idporten.eudiw.demo.verifier.config.LanguageSupportConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.LocaleResolver;
-
-import java.util.Locale;
 
 @Controller
 public class IndexController {
 
     private final ConfigProvider configProvider;
-    private final LocaleResolver localeResolver;
 
-    public IndexController(ConfigProvider configProvider, LocaleResolver localeResolver) {
+
+    public IndexController(ConfigProvider configProvider) {
         this.configProvider = configProvider;
-        this.localeResolver = localeResolver;
     }
 
     @GetMapping(value = "/")
-    public String index(@RequestParam(name = "lang", required = false) String lang, Model model, HttpServletRequest request, HttpServletResponse response) {
-        if (lang != null) {
-            Locale locale = Locale.of(lang);
-            localeResolver.setLocale(request, response, LanguageSupportConfig.SUPPORTED_LOCALES.contains(locale) ? locale : LanguageSupportConfig.DEFAULT_LOCALE);
-        }
+    public String index(Model model) {
         model.addAttribute("credentialTypes", configProvider.getCredentialConfigurations());
         return "index";
     }
