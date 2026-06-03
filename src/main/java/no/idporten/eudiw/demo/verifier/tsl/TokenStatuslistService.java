01ec4110cc246f65e860b39b8cf4699ed9b5b4e1
Solveig Langbakk <solveig.langbakk@digdir.no>
2026-06-03 15:24:36 +0200

    exception handtering mer generell for situasjoner der kommunikasjon med statusapi ikke funker

--- src/main/java/no/idporten/eudiw/demo/verifier/tsl/TokenStatuslistService.java
+++ src/main/java/no/idporten/eudiw/demo/verifier/tsl/TokenStatuslistService.java
@@ -17,6 +17,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
 
+import java.net.ConnectException;
+import java.net.SocketException;
 import java.net.URI;
 import java.text.ParseException;
 import java.security.cert.X509Certificate;
@@ -97,11 +99,9 @@ public class TokenStatuslistService {
                         .uri(url)
                         .retrieve()
                         .body(String.class);
-            } catch (RestClientResponseException e) {
-                throw new StatusCommunicationException("Could not verify status" , "Error in communication with status api "+ e.getMessage(), e.getCause());
             }
             catch (Exception e) {
-                throw new VerificationException("Invalid response" , "Error in communication with status api "+ e.getMessage());
+                throw new StatusCommunicationException("Could not verify status" , "Error in communication with status api "+ e.getMessage(), e.getCause());
             }
         } else {
             throw new VerificationException("Invalid response", "Statuslist url is null for url "+ url);
